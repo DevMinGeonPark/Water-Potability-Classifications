26595f7fd523bc2a483007988e9e00adaf3ecdfb
DevMinGeonPark <devmin7985@gmail.com>
2022-06-06 02:55:40 +0900

    add some graphs

--- auto-env-setup.sh
+++ auto-env-setup.sh
@@ -1,2 +1,2 @@
-#cat ~/.zshrc | grep __conda_setup -A 10 > auto-env-setup.sh
-#cat .commandbox >> auto-env-setup.sh
+cat ~/.zshrc | grep __conda_setup -A 10 > auto-env-setup.sh
+cat .commandbox >> auto-env-setup.sh
